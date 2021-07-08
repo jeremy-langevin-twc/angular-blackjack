e910fa163a3c6dccfb54e0ea7533132c9c6e7a0d
jeremy-langevin <jeremy.langevin@troyweb.com>
2021-07-08 14:03:38 -0400

    Getting dealer cards on init.

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -10,15 +10,22 @@ import { CardService } from '../../services/card.service';
 export class CardsComponent implements OnInit {
 
   cards: Card[] = [];
+  dealerCards: Card[] = [];
 
   constructor(private cardService: CardService) { }
 
   ngOnInit(): void {
     this.getStartingCards();
+    this.getDealerCards();
   }
 
   getStartingCards(): void {
     this.cardService.getStartingCards()
       .subscribe(cards => this.cards = cards);
+  }
+
+  getDealerCards(): void {
+    this.cardService.getDealerCards()
+      .subscribe(cards => this.dealerCards = cards);
   }
 }
