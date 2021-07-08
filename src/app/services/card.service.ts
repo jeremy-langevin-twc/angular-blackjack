dd85bd37cd9f2e7f13beb1584fb92d5c72b110f8
jeremy-langevin <jeremy.langevin@troyweb.com>
2021-07-08 14:02:24 -0400

    Added getDealerCards method.

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Card } from '../card';
-import { CARDS } from '../mock-cards';
+import { CARDS, DEALERCARDS } from '../mock-cards';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
@@ -11,6 +11,11 @@ export class CardService {
   drawn: number = 0;
 
   constructor() { }
+
+  getDealerCards(): Observable<Card[]> {
+    const cards = of(DEALERCARDS);
+    return cards;
+  }
 
   getStartingCards(): Observable<Card[]> {
     const cards = of(CARDS);
